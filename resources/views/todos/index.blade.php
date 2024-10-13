0c3b755e6c0ec10b21944ec37cf56fad2ebe8cf6
Austin Georgiades <algchoo@gmail.com>
2024-10-13 13:47:20 -0400

    start working on page styling

--- resources/views/todos/index.blade.php
+++ resources/views/todos/index.blade.php
@@ -1,26 +1,43 @@
-<form action="/todo" method="POST">
-    @csrf
-    <input type="text" name="title" placeholder="New Todo">
-    <input type="text" name="description" placeholder="Description">
-    <button type="submit">Add Todo</button>
-</form>
+@extends('layouts.app')
 
-<ul>
-    @foreach ($todos as $todo)
-        <li>
-            {{ $todo->title }} - {{ $todo->description }}
-            <form action="/todo/{{ $todo->id }}" method="POST">
-                @csrf
-                @method('PATCH')
-                <input type="hidden" name="is_complete" value="0">
-                <input type="checkbox" name="is_complete" value="1" {{ $todo->is_complete ? 'checked' : '' }}>
-                <button type="submit">Update</button>
-            </form>
-            <form action="/todo/{{ $todo->id }}" method="POST">
-                @csrf
-                @method('DELETE')
-                <button type="submit">Delete</button>
-            </form>
-        </li>
-    @endforeach
-</ul>
+@section('content')
+<div class="container">
+    <!-- Form to add a new todo -->
+    <form action="/todo" method="POST">
+        @csrf
+        <input type="text" name="title" placeholder="New Todo" class="form-input">
+        <input type="text" name="description" placeholder="Description" class="form-input">
+        <button type="submit" class="btn btn-primary">Add Todo</button>
+    </form>
+
+    <!-- List of todos -->
+    <ul class="todo-list">
+        @foreach ($todos as $todo)
+            <li class="{{ $todo->is_complete ? 'complete' : 'incomplete' }}">
+                <div class="todo-content">
+                    <span class="todo-title">{{ $todo->title }}</span> - 
+                    <span class="todo-description">{{ $todo->description }}</span>
+                </div>
+
+                <div class="todo-actions">
+                    <!-- Form to update todo (mark complete/incomplete) -->
+                    <form action="/todo/{{ $todo->id }}" method="POST">
+                        @csrf
+                        @method('PATCH')
+                        <input type="hidden" name="is_complete" value="0">
+                        <input type="checkbox" name="is_complete" value="1" {{ $todo->is_complete ? 'checked' : '' }}>
+                        <button type="submit" class="btn btn-primary">Update</button>
+                    </form>
+
+                    <!-- Form to delete todo -->
+                    <form action="/todo/{{ $todo->id }}" method="POST">
+                        @csrf
+                        @method('DELETE')
+                        <button type="submit" class="btn delete">Delete</button>
+                    </form>
+                </div>
+            </li>
+        @endforeach
+    </ul>
+</div>
+@endsection
