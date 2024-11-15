fbbaf24740c3bb041b3a810cd2d66e9f0991dde7
Austin Georgiades <algchoo@gmail.com>
2024-11-15 14:54:18 -0500

    add rating system views and styles

--- resources/views/resume/show.blade.php
+++ resources/views/resume/show.blade.php
@@ -1,5 +1,14 @@
 @extends('layouts.app')
 
 @section('content')
-<embed src="{{ asset('/ageorgiades-resume-2024.pdf') }}" type="application/pdf" width="100%" height="800px">
+<iframe src="{{ asset('/ageorgiades-resume-2024.pdf') }}" width="100%" height="600px" style="border: none;"></iframe>
+<div class="container">
+    <form action="{{ route('resume.rateResume') }}" method="POST">
+        @csrf
+        <div class="rating-container">
+            <star-rating></star-rating>
+            <button type="submit">Submit Rating</button>
+        </div>
+    </form>
+</div>
 @endsection
