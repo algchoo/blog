1b16e3f122772e7872f33febb1649eb8c0fb136c
Austin Georgiades <algchoo@gmail.com>
2024-12-20 13:23:15 -0500

    removed old scss and created new application views

---
+++ resources/views/home.blade.php
@@ -0,0 +1,16 @@
+@extends('layouts.app')
+
+@section('header')
+<h1>dumpster.zip</h1>
+<p>a tech blog</p>
+@endsection
+
+@section('main')
+<nav class="">
+    <ul>
+        <li><a href="">Writings</a></li>
+        <li><a href="">Author</a></li>
+        <li><a href="">Resume</a></li>
+    </ul>
+</nav>
+@endsection
