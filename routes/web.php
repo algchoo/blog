506a8e6629fc0a2a8a3769370e433945e81a0e50
Austin Georgiades <algchoo@gmail.com>
2024-12-22 22:27:12 -0500

    added names to all routes

--- routes/web.php
+++ routes/web.php
@@ -2,11 +2,11 @@
 
 Route::get('/', function () {
     return view('home');
-});
+})->name('home');
 
 Route::get('/author', function () {
     return view('author');
-});
+})->name('author');
 
 Route::get('/blog-posts', [BlogPostsController::class, 'index'])->name('blog.index');
 Route::get('/blog-posts/{id?}', [BlogPostsController::class, 'blogPost'])->name('blog.post');
