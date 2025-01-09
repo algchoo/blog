eeb488e55c9b91a2cf072e3c211c79f6470b8079
Austin Georgiades <algchoo@gmail.com>
2025-01-08 19:46:55 -0500

    updated vite config to output manifest

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,11 @@ import laravel from 'laravel-vite-plugin';
 import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
+    build: {
+        manifest: true,
+        outDir: 'public/build', // Adjust the output directory as needed
+    },
+    base: '/',
     plugins: [
         laravel({
             input: [
