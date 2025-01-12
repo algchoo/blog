7ca14e293d227fe3419f57c1cc1f4abf8f5ee027
Austin Georgiades <algchoo@gmail.com>
2025-01-12 10:56:28 -0500

    update styles and wrap elements in div container for homepage

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,16 +1,20 @@
 @extends('layouts.app')
 
 @section('header')
-<h1>dumpster.zip</h1>
-<p>a tech blog</p>
+<div class="home-header">
+    <h1>dumpster.zip</h1>
+    <p>a tech blog</p>
+</div>
 @endsection
 
 @section('main')
-<nav class="home-nav">
-    <ul>
-        <li><a href="{{ route('blog.index') }}">Writings</a></li>
-        <li><a href="{{ route('author') }}">Author</a></li>
-        <li><a href="#">Resume</a></li>
-    </ul>
-</nav>
+<div class="home-nav">
+    <nav>
+        <ul>
+            <li><a href="{{ route('blog.index') }}">Writings</a></li>
+            <li><a href="{{ route('author') }}">Author</a></li>
+            <li><a href="#">Resume</a></li>
+        </ul>
+    </nav>
+</div>
 @endsection
