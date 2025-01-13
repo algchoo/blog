48e6fea5d43912c147fcee7f2c34b685c3afaab2
Austin Georgiades <algchoo@gmail.com>
2025-01-12 21:03:17 -0500

    add navigation to blog post page

--- resources/views/blog/post.blade.php
+++ resources/views/blog/post.blade.php
@@ -1,12 +1,25 @@
 @extends('layouts.app')
 
 @section('header')
-<h1>{{ $blog->title }}</h1>
-<h2>{{ $blog->description }}</h2>
-<h3>Austin Georgiades</h3>
-<h4>{{ $blog->created_at->format('Y-m-d') }}</h4>
+<div class="blog-post-header">
+    <nav>
+        <ul>
+            <li><a href="{{ route('home') }}">Home</a></li>
+            <li><a href="{{ route('blog.index') }}">Writings</a></li>
+            <li><a href="{{ route('author') }}">Author</a></li>
+            <li><a href="{{ route('resume') }}" target="_blank">Resume</a></li>
+        </ul>
+    </nav>
+</div>
 @endsection
 
 @section('main')
-{!! $html !!}
+<div class="blog-post-main">
+    <h1>{{ $blog->title }}</h1>
+    <h2>{{ $blog->description }}</h2>
+    <h3>Austin Georgiades</h3>
+    <h4>{{ $blog->created_at->format('Y-m-d') }}</h4>
+
+    {!! $html !!}
+</div>
 @endsection
