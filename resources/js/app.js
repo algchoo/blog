2078ab48e245c98a4931c4e9ec34246272afa918
Austin Georgiades <algchoo@gmail.com>
2025-02-03 14:43:33 -0500

    deleted files that are not needed

--- resources/js/app.js
+++
@@ -1,8 +0,0 @@
-import './bootstrap';
-import { createApp } from 'vue';
-import StarRating from './components/StarRating.vue';
-
-// Create the Vue application instance and register the component
-const app = createApp({});
-app.component('star-rating', StarRating);
-app.mount('#app');
--- scripts/insert-blogs.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO blog_posts (title, description, markdown, created_at, updated_at)
-VALUES
-    ('Blog Post Test', 'Testing real markdown and an image', pg_read_file('/tmp/blog-test.md'), CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
