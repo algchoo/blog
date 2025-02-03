2514707e609605b71c03e87b8cbc507758db94e9
Austin Georgiades <algchoo@gmail.com>
2025-02-03 14:47:05 -0500

    remove app.js from vite configuration

--- vite.config.js
+++ vite.config.js
@@ -19,7 +19,6 @@ export default defineConfig({
         laravel({
             input: [
                 'resources/sass/app.scss',
-                'resources/js/app.js',
             ],
             refresh: true,
         }),
