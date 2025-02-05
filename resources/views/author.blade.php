582dacc942bebef948cd6488de6d2d004a1839af
Austin Georgiades <algchoo@gmail.com>
2025-02-05 17:20:36 -0500

    cleanup author section

--- resources/views/author.blade.php
+++ resources/views/author.blade.php
@@ -16,7 +16,7 @@
 @section('main')
 <div class="author-main">
     <p>
-    I’m Austin Georgiades, a software engineer with a passion for infrastructure and automation. I enjoy video games, cats, camping when it’s shit out, as well as, a variety of other activities.
+    I’m Austin Georgiades, a software engineer with a passion for infrastructure and automation. I enjoy video games, movies, having two cats, camping when it’s cold, and other pastimes.
 
     This blog is meant for me to write and share my learning/experience with others around a variety of technologies and tools. There will be a focus in the DevOps/SRE skill-set.
 
