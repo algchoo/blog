b2658b9e2c6a8365958ef03a11fd9e1a8c8676d8
Austin Georgiades <algchoo@gmail.com>
2025-03-10 13:17:22 -0400

    script to insert data into sqlite database during build

--- scripts/load-json.sh
+++ scripts/load-json.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
 
+set -e
+
 jsonPath="/var/www/public/json"
 
-for file in $jsonPath/*.json; do
-    json=$(cat $file)
+for file in "$jsonPath"/*.json; do
+    title=$(jq -r '.title' "$file") || { echo "Error parsing title in $file"; continue; }
+    description=$(jq -r '.description' "$file") || { echo "Error parsing description in $file"; continue; }
+    markdown=$(jq -r '.markdown' "$file") || { echo "Error parsing markdown in $file"; continue; }
+    db="/var/www/database/database.sqlite"
 
-    curl -X POST http://upload-md-service:8080/posts \
-        -H "Content-Type: application/json" \
-        -d "$json"
+    title=${title//\'/''}
+    description=${description//\'/''}
+    markdown=${markdown//\'/''}
 
-    sleep 1
+    sqlite3 "$db" "insert into blog_posts (title, description, markdown, created_at, updated_at) values ('$title', '$description', '$markdown', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);"
 done
