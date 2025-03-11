34fbda22b1d5e9e0f218d80a1ec47ddd48e044f1
Austin Georgiades <algchoo@gmail.com>
2025-03-10 20:00:18 -0400

    update filename for artisan migrate command in db-setup script

--- scripts/db-setup.sh
+++ scripts/db-setup.sh
@@ -4,4 +4,4 @@ set -e
 
 php artisan session:table
 php artisan migrate
-php artisan migrate --path=database/migrations/BlogPostMigration.php
+php artisan migrate --path=database/migrations/2025_03_10_205029_blog_posts.php
