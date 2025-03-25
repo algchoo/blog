894698fc8c8cfdb4c39cf2f53f67b36ad5ce51f7
Austin Georgiades <algchoo@gmail.com>
2025-03-25 15:39:35 -0400

    add terraform to deploy to cloud run

---
+++ terraform/cloud-run.tf
@@ -0,0 +1,56 @@
+provider "google" {
+    project = "dumpster-blog"
+    region = "us-central1"
+    zone = "us-central1-c"
+}
+
+data "google_secret_manager_secret_version" "blog_app_key" {
+  secret  = "blog_app_key"
+  version = "latest"
+}
+
+resource "google_secret_manager_secret_iam_member" "secret_access" {
+  secret_id = data.google_secret_manager_secret_version.blog_app_key.secret
+  role      = "roles/secretmanager.secretAccessor"
+  member    = "serviceAccount:${google_cloud_run_service.my_service.template[0].spec[0].service_account_name}"
+}
+
+resource "google_cloud_run_v2_service" "default" {
+  name     = "cloudrun-service"
+  location = "us-central1"
+  deletion_protection = false
+  ingress = "INGRESS_TRAFFIC_ALL"
+
+  template {
+    containers {
+      name = "blog"
+      image = "ghcr.io/algchoo/blog:0739f4e"
+      env {
+        name = "APP_KEY"
+        value_source {
+          secret_key_ref {
+            secret = google_secret_manager_secret_version.blog_app_key.secret
+            version = "latest"
+          }
+        }
+      }
+      resources {
+        limits = {
+          cpu    = "2"
+          memory = "1024Mi"
+        }
+      }
+    }
+    containers {
+      name = "nginx"
+      image = "ghcr.io/algchoo/nginx:0739f4e"
+      depends_on = "blog"
+      resources {
+        limits = {
+          cpu    = "2"
+          memory = "1024Mi"
+        }
+      }
+    }
+  }
+}
